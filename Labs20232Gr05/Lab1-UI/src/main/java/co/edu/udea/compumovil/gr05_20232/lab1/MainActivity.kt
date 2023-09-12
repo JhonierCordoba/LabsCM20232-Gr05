8a8a92b44ede403af3983fe0f9012ef69d01afcb
Jhonier Cordoba <jhonier.cordoba@udea.edu.co>
2023-09-11 20:40:04 -0500

    agrega lenguaje alternativo

--- Labs20232Gr05/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20232/lab1/MainActivity.kt
+++ Labs20232Gr05/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20232/lab1/MainActivity.kt
@@ -1,43 +1,80 @@
 package co.edu.udea.compumovil.gr05_20232.lab1
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import co.edu.udea.compumovil.gr05_20232.lab1.ui.theme.Labs20232Gr05Theme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Labs20232Gr05Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    EditTextExample()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun EditTextExample() {
+    // ViewModel to hold the text state
+    val viewModel: EditTextViewModel = viewModel()
+    var texto: String
+
+    Column {
+        TextField(
+            label = { Text("Text") },
+            value = viewModel.textState.value,
+            onValueChange = {
+                viewModel.textState.value = it
+                texto = it.toString()
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .border(1.dp, Color.Gray)
+                .padding(16.dp)
+        )
+
+        Text(
+            text = "You typed: ${viewModel.textState.value}",
+            fontSize = 16.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Labs20232Gr05Theme {
-        Greeting("Android")
-    }
+// ViewModel to hold the text state
+class EditTextViewModel : ViewModel() {
+    val textState = mutableStateOf("")
 }
