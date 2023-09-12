46566fef87622d10e24632ac2dc7d4d3f8a6ca69
Jhonier Cordoba <jhonier.cordoba@udea.edu.co>
2023-09-11 21:51:34 -0500

    constrain layout

--- Labs20232Gr05/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20232/lab1/MainActivity.kt
+++ Labs20232Gr05/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20232/lab1/MainActivity.kt
@@ -1,32 +1,27 @@
 package co.edu.udea.compumovil.gr05_20232.lab1
 
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import co.edu.udea.compumovil.gr05_20232.lab1.ui.theme.Labs20232Gr05Theme
-import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,40 +32,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    EditTextExample()
+                    EditTextExample(getString(R.string.hello))
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun EditTextExample() {
-    // ViewModel to hold the text state
-    val viewModel: EditTextViewModel = viewModel()
-    var texto: String
+fun EditTextExample(texto: String) {
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val viewModel: EditTextViewModel = viewModel()
 
-    Column {
+        val (nameTextField, nameText, boxBlue) = createRefs()
+
         TextField(
-            label = { Text("Text") },
+            label = { Text(texto) },
             value = viewModel.textState.value,
             onValueChange = {
                 viewModel.textState.value = it
-                texto = it.toString()
             },
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth()
                 .border(1.dp, Color.Gray)
                 .padding(16.dp)
+                .constrainAs(nameTextField) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
         )
 
-        Text(
-            text = "You typed: ${viewModel.textState.value}",
-            fontSize = 16.sp,
-            modifier = Modifier.padding(16.dp)
-        )
+        Text(modifier = Modifier.constrainAs(nameText) {
+            top.linkTo(parent.bottom)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }, text = "Text")
+
+        Text(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }, text = "nameText")
     }
 }
 
